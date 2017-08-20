6c78759bc9773fe0312a75d18bfe402140bc4e01
mEcOnNectEd <31171476+mEcOnNectEd@users.noreply.github.com>
2017-08-20 11:44:19 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -26,7 +26,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
-}else if(($arrJson['events'][0]['message']['text'] == strtoupper("Turn light on")) || ($arrJson['events'][0]['message']['text'] == "เปิดไฟ")){
+}else if((strtoupper$arrJson['events'][0]['message']['text']) == "TURN LIGHT ON") || ($arrJson['events'][0]['message']['text'] == "เปิดไฟ")){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
